943a7a1549e2f22a58e48f668e37d896035602ec
Michael Perrotte <mike@mikecorp.ca>
2016-01-05 23:34:33 -0500

    created control flow for collins

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-const collins = require('./libs/bot');
+const config = require('./config');
+const Collins = require('./libs/bot');
+
+
+let collins = new Collins(config);
+
+collins.start(function(err) {
+  console.log('collins is at your service.');
+});
