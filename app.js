f86f11ac2ec20f690bc356bc151ebba3c8630a3e
Michael Perrotte <mike@mikecorp.ca>
2016-01-11 04:11:37 -0500

    added comments to app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 'use strict';
 
+// INFO: require configuration file/folder
 const config = require('./config');
+
+// INFO: require collins module
 const Collins = require('./libs/collins');
 
-
+// INFO: instantiate collins with config
 let collins = new Collins(config);
+
+// INFO: require plugin
+// const tenMan = require('./plugins/tenMan');
+
+// INFO: use plugin with collins
+// collins.use(tenMan);
 
 collins.start(function(err) {
   console.log('collins is at your service.');
