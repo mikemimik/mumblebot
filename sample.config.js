3d75693dbebda94a971a73b418bcc37c9ef33009
Michael Perrotte <mike@mikecorp.ca>
2016-01-11 15:37:26 -0500

    created sample config file

---
+++ sample.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  server: 'mumble://server-host:server-port',
+  username: 'bot-username',
+  passowrd: 'mumble-password',
+  plugins: [],
+  ssl: {
+    key: 'path/to/ssl/key.pem',
+    cert: 'path/to/ssl/cert.pem'
+  },
+  debug: false
+};
