f815f3a2be3b5d6d7d46ca0214425a2d1eaf88b2
Michael Perrotte <mike@mikecorp.ca>
2016-01-25 01:58:32 -0500

    created second test plugin, to be loaded differently

---
+++ plugins/meetups.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports ={
+  name: 'meetup',
+  description: 'helping people meet.',
+  triggers: {
+  'meetup': 'Many of us have met in person, usually for drinks. Join #meetups to coordinate!'
+  }
+};
