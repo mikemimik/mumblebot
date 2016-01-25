efb28e488aed085a06d46bf6781adaf185e64a17
Michael Perrotte <mike@mikecorp.ca>
2016-01-25 14:51:34 -0500

    removed some commnets

--- app.js
+++ app.js
@@ -15,16 +15,6 @@ const tenMan = require('./plugins/tenMan');
 // INFO: use plugin with collins
 collins.use(tenMan);
 
-// INFO: describe what happens after you initialize collins
-// collins.on('loaded', (err, self) => {
-//   self.log('I\'ve successfully inicialized.');
-
-//   collins.start(function(err) {
-//     console.log('collins is at your service.');
-//   });
-// });
-
-/* TESTING */
 collins.on('initialized', (err, context) => {
   console.log('>>', 'EVENT', 'initialized');
   collins.start();
