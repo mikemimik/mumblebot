981c93f171e8f1d2986414bc4dca8941cb1eacc2
Michael Perrotte <mike@mikecorp.ca>
2016-01-24 23:27:09 -0500

    refactored plugin structure

--- plugins/tenMan.js
+++ plugins/tenMan.js
@@ -1,7 +1,11 @@
 'use strict';
 
 module.exports ={
+  name: 'tenMan',
+  description: 'some stuff about this thing.',
+  triggers: {
   '10man': function() {},
   '10man.start': function() {},
   '10man.stop': function() {}
+  }
 };
